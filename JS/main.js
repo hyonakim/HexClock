fa7161a96ac519fa9031e2230d1807bc81ab1618
Hyona Kim <tekkieninja@gmail.com>
2015-10-05 15:20:38 -0400

    added code to jquery.js

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+setInterval(function(){
+
+var date = new Date();
+var hour = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+
+var hr = (hr < 10) ? '0' + hour : hour; 
+var min = (minutes < 10) ? '0' + minutes : minutes;
+var sec = (seconds < 10) ? '0' + seconds : seconds;
+var totalTime = hr + ':' + min + ':' + sec;
+
+$('#clock').text(totalTime);
+},1000);
